664a055ac13f3811a74267ac5a4bf60446301164
vs002666 <ASHISH_AKSHANTAL@neimanmarcus.com>
2023-05-01 11:55:36 -0500

    changed reference to housebill in json payload

--- src/shared/joiSchema.js
+++ src/shared/joiSchema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const locationUpdateSchema = Joi.object().keys({
-  reference: Joi.string().required(),
+  housebill: Joi.string().required(),
   location: Joi.object({
     latitude: Joi.number().required(),
     longitude: Joi.number().required(),
