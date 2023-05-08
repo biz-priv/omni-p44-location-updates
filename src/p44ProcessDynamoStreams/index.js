1908cdce037437789491520c734e5340ebe66e46
Abdul-Rashed <122110062+Abdul-Rashed@users.noreply.github.com>
2023-05-08 22:09:01 +0530

    added code for process dynamo streams

--- src/p44ProcessDynamoStreams/index.js
+++ src/p44ProcessDynamoStreams/index.js
@@ -1,3 +1,45 @@
-exports.handler = async (event, context, callback) => {
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const { CUSTOMER_MCKESSON } = process.env
+
+module.exports.handler = async (event, context) => {
   console.log("event", JSON.stringify(event));
+  try {
+
+    if (event.Records[0].eventName !== 'INSERT') {
+      return;
+    }
+
+    const houseBill = event.Records[0].dynamodb.NewImage.HouseBillNo.S;
+
+    const params = {
+      TableName: process.env.SHIPMENT_HEADER_TABLE,
+      Key: {
+        BillNo: houseBill
+      }
+    };
+
+    const response = await dynamoDB.get(params).promise();
+
+    if (response.Item.Customer !== CUSTOMER_MCKESSON) {
+      return;
+    }
+
+    const newTable = 'omni-p44-location-sf-status-dev';
+
+    const newItem = {
+      HouseBillNo: houseBill,
+      StepFunctionStatus: 'Yet to be Processed'
+    };
+
+    const putParams = {
+      TableName: newTable,
+      Item: newItem
+    };
+
+    await dynamoDB.put(putParams).promise();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
