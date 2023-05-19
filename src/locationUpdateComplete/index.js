ebc5eec331a26b92edb08a2d51674891a5d81f6d
Dsish <debasish@yeasitech.com>
2023-05-19 14:39:10 +0530

    update location-update & sf-status ddb

--- src/locationUpdateComplete/index.js
+++ src/locationUpdateComplete/index.js
@@ -1,11 +1,105 @@
+const { marshall } = require("@aws-sdk/util-dynamodb");
 const { response } = require("../shared/helper");
 const { log, logUtilization } = require("../shared/logger");
+const {
+  delete_dynamo_item,
+  put_dynamo,
+  update_dynamo_item,
+} = require("../shared/dynamoDb");
+const { P44_SF_STATUS_TABLE, P44_LOCATION_UPDATE_TABLE } = process.env;
+
+//=============>
+const AWS = require("aws-sdk");
+var ddb = new AWS.DynamoDB.DocumentClient();
+let items;
+let queryResults = [];
 
 module.exports.handler = async (event, context, callback) => {
   console.log("Event", JSON.stringify(event));
-  try {
-    return callback(response("[200]", "Statue Update Complete"));
-  } catch (error) {
-    return callback(response("[400]", "Statue Update Failed"));
+
+  const records = event.Records;
+  const houseBill = records[0].dynamodb.NewImage.HouseBillNo.S;
+  const shipmentStatus = records[0].dynamodb.NewImage.ShipmentStatus.S;
+
+  if (shipmentStatus === "Pending") {
+    try {
+      let sfDynamoPayload = {
+        HouseBillNo: houseBill,
+        StepFunctionStatus: "Complete",
+      };
+      sfDynamoPayload = marshall(sfDynamoPayload);
+      const sfDltParams = {
+        TableName: P44_SF_STATUS_TABLE,
+        Key: {
+          HouseBillNo: { S: houseBill },
+          StepFunctionStatus: { S: "Pending" },
+        },
+      };
+      const sfParams = {
+        TableName: P44_SF_STATUS_TABLE,
+        Item: sfDynamoPayload,
+      };
+
+      console.log("sfDltParams", sfDltParams);
+      console.log("sfParams", sfParams);
+      const sfDlt = await delete_dynamo_item(sfDltParams);
+      const sfResp = await put_dynamo(sfParams);
+      console.log("Udated Successfully in P44_SF_STATUS_TABLE", locationResp);
+
+      const params = {
+        TableName: P44_LOCATION_UPDATE_TABLE,
+        IndexName: "shipment-status-index-dev",
+        KeyConditionExpression: "ShipmentStatus = :pk",
+        FilterExpression: "HouseBillNo = :val",
+        ExpressionAttributeValues: {
+          ":pk": "Pending",
+          ":val": houseBill,
+        },
+        limit: 100,
+      };
+
+      do {
+        items = await ddb.query(params).promise();
+        items.Items.forEach((item) => queryResults.push(item));
+        params.ExclusiveStartKey = items.LastEvaluatedKey;
+      } while (typeof items.LastEvaluatedKey != "undefined");
+
+      console.log("locationData", JSON.stringify(queryResults));
+
+      let locationResp;
+      if (queryResults.length > 0) {
+        for (let i = 0; i < queryResults.length; i++) {
+          const utcTimeStamp = queryResults[i].UTCTimeStamp;
+          correlationId = queryResults[i].CorrelationId;
+          log(correlationId, JSON.stringify(correlationId), 200);
+
+          console.log(`utcTimeStamp${i}=====>`, utcTimeStamp);
+          const locationParams = {
+            TableName: P44_LOCATION_UPDATE_TABLE,
+            Key: {
+              HouseBillNo: { S: houseBill },
+              UTCTimeStamp: { S: utcTimeStamp },
+            },
+            UpdateExpression: "SET #attr = :val",
+            ExpressionAttributeNames: { "#attr": "ShipmentStatus" },
+            ExpressionAttributeValues: {
+              ":val": { S: "Pending" },
+            },
+          };
+          locationResp = await update_dynamo_item(locationParams);
+        }
+      }
+      console.log(
+        "Udated Successfully in P44_LOCATION_UPDATE_TABLE",
+        locationResp
+      );
+      log(correlationId, JSON.stringify(event), 200);
+      log(correlationId, JSON.stringify(locationResp), 200);
+      await logUtilization(correlationId);
+      return { Msg: "Statue Update Success" };
+    } catch (error) {
+      console.log("Error", error);
+      return callback(response("[400]", "Statue Update Failed"));
+    }
   }
 };
