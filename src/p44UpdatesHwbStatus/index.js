d41b2b533a7b004303ec1200a3dcd2117ed2e289
Dsish <debasish@yeasitech.com>
2023-05-19 23:24:25 +0530

    update location-update & sf-status ddb

--- src/p44UpdatesHwbStatus/index.js
+++ src/p44UpdatesHwbStatus/index.js
@@ -22,105 +22,104 @@ module.exports.handler = async (event, context, callback) => {
   console.log("event==>", typeof event);
   const stepEvent = event;
 
-  // try {
-  //   let newImage = stepEvent.Records[0].dynamodb.NewImage;
-  //   let correlationId = "";
-  //   let keys = stepEvent.Records[0].dynamodb.Keys;
-  //   const houseBill = keys.HouseBillNo.S;
-  //   const sfStatus = keys.StepFunctionStatus.S;
-  //   let locationStatus = "";
+  try {
+    let keys = stepEvent.Records[0].dynamodb.Keys;
+    const houseBill = keys.HouseBillNo.S;
+    const sfStatus = keys.StepFunctionStatus.S;
+    let locationStatus = "";
+    let correlationId = "";
 
-  //   if (keys.StepFunctionStatus.S === "Yet to be Processed") {
-  //     locationStatus = "In-Complete";
-  //   }
+    if (keys.StepFunctionStatus.S === "Yet to be Processed") {
+      locationStatus = "In-Complete";
+    }
 
-  //   // For SF_Status--------------------------------------------------------------------->
-  //   let sfDynamoPayload = {
-  //     HouseBillNo: houseBill,
-  //     StepFunctionStatus: "Pending",
-  //   };
-  //   sfDynamoPayload = marshall(sfDynamoPayload);
+    // For SF_Status--------------------------------------------------------------------->
+    let sfDynamoPayload = {
+      HouseBillNo: houseBill,
+      StepFunctionStatus: "Pending",
+    };
+    sfDynamoPayload = marshall(sfDynamoPayload);
 
-  //   const sfDltParams = {
-  //     TableName: P44_SF_STATUS_TABLE,
-  //     Key: {
-  //       HouseBillNo: { S: houseBill },
-  //       StepFunctionStatus: { S: sfStatus },
-  //     },
-  //   };
-  //   const sfParams = {
-  //     TableName: P44_SF_STATUS_TABLE,
-  //     Item: sfDynamoPayload,
-  //   };
+    const sfParams = {
+      TableName: P44_SF_STATUS_TABLE,
+      Item: sfDynamoPayload,
+    };
 
-  //   // Query Location updates --------------------------------------------------------------------------------------------->
-  //   const params = {
-  //     TableName: P44_LOCATION_UPDATE_TABLE,
-  //     IndexName: "shipment-status-index-dev",
-  //     KeyConditionExpression: "ShipmentStatus = :pk",
-  //     FilterExpression: "HouseBillNo = :val",
-  //     ExpressionAttributeValues: {
-  //       ":pk": locationStatus,
-  //       ":val": houseBill,
-  //     },
-  //     limit: 100,
-  //   };
-  //   console.log("Location_Query_Params", params);
+    const sfDltParams = {
+      TableName: P44_SF_STATUS_TABLE,
+      Key: {
+        HouseBillNo: { S: houseBill },
+        StepFunctionStatus: { S: sfStatus },
+      },
+    };
+    // Query Location updates --------------------------------------------------------------------------------------------->
+    const params = {
+      TableName: P44_LOCATION_UPDATE_TABLE,
+      IndexName: "shipment-status-index-dev",
+      KeyConditionExpression: "ShipmentStatus = :pk",
+      FilterExpression: "HouseBillNo = :val",
+      ExpressionAttributeValues: {
+        ":pk": locationStatus,
+        ":val": houseBill,
+      },
+      limit: 100,
+    };
+    console.log("Location_Query_Params", params);
 
-  //   do {
-  //     items = await ddb.query(params).promise();
-  //     items.Items.forEach((item) => queryResults.push(item));
-  //     params.ExclusiveStartKey = items.LastEvaluatedKey;
-  //   } while (typeof items.LastEvaluatedKey != "undefined");
+    do {
+      items = await ddb.query(params).promise();
+      items.Items.forEach((item) => queryResults.push(item));
+      params.ExclusiveStartKey = items.LastEvaluatedKey;
+    } while (typeof items.LastEvaluatedKey != "undefined");
 
-  //   const locationData = queryResults;
-  //   console.log("locationData", JSON.stringify(locationData));
-  //   //--------------------------------------------------------------------------------------------->
+    const locationData = queryResults;
+    console.log("locationData", JSON.stringify(locationData));
+    //--------------------------------------------------------------------------------------------->
 
-  //   // console.log("sfParams", sfParams);
-  //   // console.log("locationParams", locationParams);
+    // console.log("sfParams", sfParams);
+    // console.log("locationParams", locationParams);
 
-  //   // Update SF_Status---------------------------------------------------------------------------->
-  //   const sfDlt = await delete_dynamo_item(sfDltParams);
-  //   const sfResp = await put_dynamo(sfParams);
-  //   console.log("Udated Successfully in P44_SF_STATUS_TABLE", sfResp);
+    // Update SF_Status---------------------------------------------------------------------------->
+    const sfDlt = await delete_dynamo_item(sfDltParams);
+    const sfResp = await put_dynamo(sfParams);
+    console.log("Udated Successfully in P44_SF_STATUS_TABLE", sfResp);
 
-  //   // Update Location_Updates---------------------------------------------------------------------------->
-  //   let locationResp;
-  //   if (locationData.length > 0) {
-  //     for (let i = 0; i < locationData.length; i++) {
-  //       const utcTimeStamp = locationData[i].UTCTimeStamp;
-  //       correlationId = locationData[i].CorrelationId;
-  //       log(correlationId, JSON.stringify(correlationId), 200);
+    // Update Location_Updates----------------------------------------------------------------------->
+    let locationResp;
+    if (locationData.length > 0) {
+      for (let i = 0; i < locationData.length; i++) {
+        const utcTimeStamp = locationData[i].UTCTimeStamp;
+        correlationId = locationData[i].CorrelationId;
+        log(correlationId, JSON.stringify(correlationId), 200);
 
-  //       console.log(`utcTimeStamp${i}=====>`, utcTimeStamp);
-  //       const locationParams = {
-  //         TableName: P44_LOCATION_UPDATE_TABLE,
-  //         Key: {
-  //           HouseBillNo: { S: houseBill },
-  //           UTCTimeStamp: { S: utcTimeStamp },
-  //         },
-  //         UpdateExpression: "SET #attr = :val",
-  //         ExpressionAttributeNames: { "#attr": "ShipmentStatus" },
-  //         ExpressionAttributeValues: {
-  //           ":val": { S: "Pending" },
-  //         },
-  //       };
-  //       locationResp = await update_dynamo_item(locationParams);
-  //     }
-  //   }
+        console.log(`utcTimeStamp ${i}=====>`, utcTimeStamp);
+        const locationParams = {
+          TableName: P44_LOCATION_UPDATE_TABLE,
+          Key: {
+            HouseBillNo: { S: houseBill },
+            UTCTimeStamp: { S: utcTimeStamp },
+          },
+          UpdateExpression: "SET #attr = :val",
+          ExpressionAttributeNames: { "#attr": "ShipmentStatus" },
+          ExpressionAttributeValues: {
+            ":val": { S: "Pending" },
+          },
+        };
+        locationResp = await update_dynamo_item(locationParams);
+      }
+    }
 
-  //   console.log(
-  //     "Udated Successfully in P44_LOCATION_UPDATE_TABLE",
-  //     locationResp
-  //   );
-  //   log(correlationId, JSON.stringify(event), 200);
-  //   log(correlationId, JSON.stringify(locationResp), 200);
-  //   await logUtilization(correlationId);
+    console.log(
+      "Udated Successfully in P44_LOCATION_UPDATE_TABLE",
+      locationResp
+    );
+    log(correlationId, JSON.stringify(event), 200);
+    log(correlationId, JSON.stringify(locationResp), 200);
+    await logUtilization(correlationId);
 
-  //   return { houseBill };
-  // } catch (error) {
-  //   console.log("Error", error);
-  //   return callback(response("[400]", "First Lambda Failed"));
-  // }
+    return { houseBill };
+  } catch (error) {
+    console.log("Error", error);
+    return callback(response("[400]", "First Lambda Failed"));
+  }
 };
