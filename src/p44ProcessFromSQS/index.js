69cae2f4174d2aca8d538c7818b003d8a6e536fe
Dsish <debasish@yeasitech.com>
2023-05-19 20:58:41 +0530

    update location-update & sf-status ddb

--- src/p44ProcessFromSQS/index.js
+++ src/p44ProcessFromSQS/index.js
@@ -6,59 +6,51 @@ const { put_dynamo } = require("../shared/dynamoDb");
 const { log, logUtilization } = require("../shared/logger");
 const { response } = require("../shared/helper");
 
-const { P44_LOCATION_UPDATE_TABLE, P44_SQS_QUEUE_URL } = process.env;
+const { P44_LOCATION_UPDATE_TABLE } = process.env;
 
 exports.handler = async (event) => {
   console.log("event", JSON.stringify(event));
-  const params = {
-    QueueUrl: P44_SQS_QUEUE_URL,
-    MaxNumberOfMessages: 10,
-    VisibilityTimeout: 30,
-    WaitTimeSeconds: 0,
-  };
-
+  const records = event.Records;
   try {
-    // const data = await sqs.receiveMessage(params).promise();
-    // if (data.Messages) {
-    //   console.log(`Received ${data.Messages.length} messages:`);
-    //   data.Messages.forEach((message) => {
-    //     console.log(message.Body);
-    //   });
-    // } else {
-    //   console.log("No messages received");
-    // }
+    for (let i = 0; i < records.length; i++) {
+      console.log("loopCount==>", i);
 
-    let dynamoPayload = JSON.parse(event.Records[0].body);
-    const correlationId = dynamoPayload.correlationId;
-    await logUtilization(correlationId);
+      try {
+        let dynamoPayload = JSON.parse(event.Records[i].body);
+        const correlationId = dynamoPayload.correlationId;
+        await logUtilization(correlationId);
 
-    dynamoPayload = {
-      HouseBillNo: dynamoPayload.housebill,
-      UTCTimeStamp: dynamoPayload.UTCTimestamp,
-      CorrelationId: dynamoPayload.correlationId,
-      InsertedTimeStamp: moment
-        .tz("America/Chicago")
-        .format("YYYY:MM:DD HH:mm:ss")
-        .toString(),
-      ShipmentStatus: "In-Complete",
-      latitude: dynamoPayload.location.latitude,
-      longitude: dynamoPayload.location.longitude,
-    };
-    dynamoPayload = marshall(dynamoPayload);
-    console.log("dynamoPayload", dynamoPayload);
-    log(correlationId, JSON.stringify(dynamoPayload), 200);
+        dynamoPayload = {
+          HouseBillNo: dynamoPayload.housebill,
+          UTCTimeStamp: dynamoPayload.UTCTimestamp,
+          CorrelationId: dynamoPayload.correlationId,
+          InsertedTimeStamp: moment
+            .tz("America/Chicago")
+            .format("YYYY:MM:DD HH:mm:ss")
+            .toString(),
+          ShipmentStatus: "In-Complete",
+          latitude: dynamoPayload.location.latitude,
+          longitude: dynamoPayload.location.longitude,
+        };
+        dynamoPayload = marshall(dynamoPayload);
+        // console.log("dynamoPayload", dynamoPayload);
+        log(correlationId, JSON.stringify(dynamoPayload), 200);
 
-    const dynamoParams = {
-      TableName: P44_LOCATION_UPDATE_TABLE,
-      Item: dynamoPayload,
-    };
+        const dynamoParams = {
+          TableName: P44_LOCATION_UPDATE_TABLE,
+          Item: dynamoPayload,
+        };
 
-    console.log(dynamoParams);
-    log(correlationId, JSON.stringify(dynamoParams), 200);
+        console.log("dynamoParams", dynamoParams);
+        log(correlationId, JSON.stringify(dynamoParams), 200);
 
-    const res = await put_dynamo(dynamoParams);
-    console.log("res", JSON.stringify(res));
-    log(correlationId, JSON.stringify(res), 200);
+        const res = await put_dynamo(dynamoParams);
+        console.log("res", JSON.stringify(res));
+        log(correlationId, JSON.stringify(res), 200);
+      } catch (error) {
+        throw error;
+      }
+    }
   } catch (error) {
     console.log("Error", error);
     return callback(response("[400]", error));
