d9405fd1b400b63b2e55fd04c90e996b3fc5623d
Dsish <debasish@yeasitech.com>
2023-05-19 22:28:01 +0530

    update location-update & sf-status ddb

--- src/p44ProcessDynamoStreams/index.js
+++ src/p44ProcessDynamoStreams/index.js
@@ -6,102 +6,6 @@ const { CUSTOMER_MCKESSON, SHIPMENT_HEADER_TABLE, P44_SF_STATUS_TABLE } =
   process.env;
 
 module.exports.handler = async (event, context, callback) => {
-  event = {
-    Records: [
-      {
-        eventID: "7aa1651cffbd4d2afb4156f3c04906d1",
-        eventName: "INSERT",
-        eventVersion: "1.1",
-        eventSource: "aws:dynamodb",
-        awsRegion: "us-east-1",
-        dynamodb: {
-          ApproximateCreationDateTime: 1684510657,
-          Keys: {
-            UTCTimeStamp: {
-              S: "2023-05-19T15:26:00",
-            },
-            HouseBillNo: {
-              S: "74354",
-            },
-          },
-          NewImage: {
-            ShipmentStatus: {
-              S: "In-Complete",
-            },
-            UTCTimeStamp: {
-              S: "2023-05-19T15:26:00",
-            },
-            InsertedTimeStamp: {
-              S: "2023:05:19 10:37:37",
-            },
-            latitude: {
-              N: "33.064756",
-            },
-            CorrelationId: {
-              S: "4a79efad-1a58-4f22-ba5c-c5cd45ae9686",
-            },
-            HouseBillNo: {
-              S: "74354",
-            },
-            longitude: {
-              N: "-89.904472",
-            },
-          },
-          SequenceNumber: "52616600000000036816077211",
-          SizeBytes: 232,
-          StreamViewType: "NEW_AND_OLD_IMAGES",
-        },
-        eventSourceARN:
-          "arn:aws:dynamodb:us-east-1:332281781429:table/omni-p44-shipment-location-updates-dev/stream/2023-05-08T15:17:39.737",
-      },
-      {
-        eventID: "7aa1651cffbd4d2afb4156f3c04906d1",
-        eventName: "INSERT",
-        eventVersion: "1.1",
-        eventSource: "aws:dynamodb",
-        awsRegion: "us-east-1",
-        dynamodb: {
-          ApproximateCreationDateTime: 1684510657,
-          Keys: {
-            UTCTimeStamp: {
-              S: "2023-05-19T15:26:00",
-            },
-            HouseBillNo: {
-              S: "74354",
-            },
-          },
-          NewImage: {
-            ShipmentStatus: {
-              S: "In-Complete",
-            },
-            UTCTimeStamp: {
-              S: "2023-05-19T15:26:00",
-            },
-            InsertedTimeStamp: {
-              S: "2023:05:19 10:37:37",
-            },
-            latitude: {
-              N: "33.064756",
-            },
-            CorrelationId: {
-              S: "4a79efad-1a58-4f22-ba5c-c5cd45ae9686",
-            },
-            HouseBillNo: {
-              S: "74354",
-            },
-            longitude: {
-              N: "-89.904472",
-            },
-          },
-          SequenceNumber: "52616600000000036816077211",
-          SizeBytes: 232,
-          StreamViewType: "NEW_AND_OLD_IMAGES",
-        },
-        eventSourceARN:
-          "arn:aws:dynamodb:us-east-1:332281781429:table/omni-p44-shipment-location-updates-dev/stream/2023-05-08T15:17:39.737",
-      },
-    ],
-  };
   console.log("event", JSON.stringify(event));
   const customerIds = CUSTOMER_MCKESSON.split(",");
   const record = event.Records;
@@ -151,7 +55,7 @@ module.exports.handler = async (event, context, callback) => {
 
               console.log("dynamoParams", JSON.stringify(dynamoParams));
 
-              // const res = await put_dynamo(dynamoParams);
+              const res = await put_dynamo(dynamoParams);
               // console.log("response", res);
             }
           } else {
